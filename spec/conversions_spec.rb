6aef3d763e23c0ad8436205437228139ca99a499
Delia Constantino <dconstan92@gmail.com>
2020-10-21 23:09:49 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -8,3 +8,12 @@ describe "conversions" do
     end
   end
 end
+
+describe "conversions" do
+  describe '#ounces_to_grams' do
+    it 'given 2, returns 0.0' do
+      grams = ounces_to_grams(0)
+      expect(grams).to eq(0.0)
+    end
+  end
+end
