325624ab2592308bd8b57a7ff61f3d1705c93052
Delia Constantino <dconstan92@gmail.com>
2020-10-21 23:10:33 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -11,9 +11,9 @@ end
 
 describe "conversions" do
   describe '#ounces_to_grams' do
-    it 'given 2, returns 0.0' do
-      grams = ounces_to_grams(0)
-      expect(grams).to eq(0.0)
+    it 'given 2, returns 56.699' do
+      grams = ounces_to_grams(2)
+      expect(grams).to eq(56.699)
     end
   end
 end
